9ca4e680809cb7c68ecaf4dcff0f2d4a048daeb4
PRAVEEN <praveen@example.com>
2026-05-08 01:04:02 +0530

    feat: add Expo Router tab navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function Layout() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="discover" options={{ title: 'Discover' }} />
+      <Tab.Screen name="browse" options={{ title: 'Browse' }} />
+      <Tab.Screen name="compare" options={{ title: 'Compare' }} />
+      <Tab.Screen name="profile" options={{ title: 'Profile' }} />
+    </Tab.Navigator>
+  );
+}
